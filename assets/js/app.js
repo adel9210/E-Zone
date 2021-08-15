57e82f9e5bdc45a4dab7099afcdf74549f671938
adel.sadek <adel.sadek@ihorizons.com>
2021-08-16 00:50:47 +0200

    feat new section

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,6 +35,72 @@ jQuery(document).ready(function ($) {
     },
   });
 
+  const news__swiper = new Swiper(".news__swiper", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    observer: true,
+    observeParents: true,
+    navigation: {
+      nextEl: ".news__swiper__wrapper .swiper-button-next",
+      prevEl: ".news__swiper__wrapper .swiper-button-prev",
+    },
+
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      // when window width is >= 991px
+      991: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+  });
+
+  const testimonials__swiper = new Swiper(".testimonials__swiper", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".testimonials__swiper__controls .swiper-button-next",
+      prevEl: ".testimonials__swiper__controls .swiper-button-prev",
+    },
+
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      // when window width is >= 991px
+      991: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+  });
+
   // REMOVE SPINNER
   setTimeout(() => {
     $(".loader").fadeOut("slow");
